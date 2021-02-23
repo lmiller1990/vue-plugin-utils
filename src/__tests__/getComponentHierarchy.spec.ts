0d3cf294712f9579d1d66d1a5b50a19b9b8ae49f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-02-23 22:45:51 +1000

    add a basic traverse

--- src/__tests__/getComponentHierarchy.spec.ts
+++ src/__tests__/getComponentHierarchy.spec.ts
@@ -1,17 +1,18 @@
 import { createApp, defineComponent, h } from 'vue'
-import { getComponentHierarchy } from '../getComponentHierarchy'
+import { getComponentHierarchy, FoundComponents } from '../getComponentHierarchy'
 
+
+const createChild = (name: string) => defineComponent({
+  name,
+  render() {
+    return h('div', `Name is ${name}`)
+  }
+})
 
 describe('getComponentHierarchy', () => {
   it('works for 1 child', () => {
-    const Child = defineComponent({
-      name: 'Child',
-      render() {
-        return h('div', 'Child')
-      }
-    })
+    const Child = createChild('Child')
     const App = defineComponent({
-      components: { Child },
       render() {
         return h('div', [h(Child)])
       }
@@ -20,6 +21,97 @@ describe('getComponentHierarchy', () => {
     const mounted = vm.mount(document.createElement('div'))
     const result = getComponentHierarchy(mounted)
 
-    console.log(result)
+    expect(result).toEqual<FoundComponents>({
+      1: {
+        parent: 0,
+        name: 'Child',
+        uid: 1
+      }
+    })
+  })
+
+  it('works for 2 levels of children', () => {
+    const Grandchild = defineComponent({
+      name: 'Grandchild',
+      render() { return h('div', 'Grandchild') }
+    })
+    const Child = defineComponent({
+      name: 'Child',
+      render() { 
+        return h('div', [h(Grandchild)])
+      }
+    })
+    const App = defineComponent({
+      render() {
+        return h('div', [h(Child)])
+      }
+    })
+    const vm = createApp(App)
+    const mounted = vm.mount(document.createElement('div'))
+    const result = getComponentHierarchy(mounted)
+
+    expect(result).toEqual<FoundComponents>({
+      1: {
+        parent: 0,
+        name: 'Child',
+        uid: 1
+      },
+      2: {
+        name: 'Grandchild',
+        parent: 1,
+        uid: 2,
+      },
+    })
+  })
+
+  it.only('works using the same components multiple times', () => {
+    const Grandchild = defineComponent({
+      name: 'Grandchild',
+      render() { return h('div', 'Grandchild') }
+    })
+    const Child = defineComponent({
+      name: 'Child',
+      render() { 
+        return h('div', [h(Grandchild)])
+      }
+    })
+    const App = defineComponent({
+      render() {
+        return h('div', [h(Child), h(Child), h(Grandchild)])
+      }
+    })
+    const vm = createApp(App)
+    const mounted = vm.mount(document.createElement('div'))
+    const result = getComponentHierarchy(mounted)
+
+    expect(result).toEqual<FoundComponents>(
+      {
+        1: {
+          name: 'Child',
+          parent: 0,
+          uid: 1,
+        },
+        2: {
+          name: 'Grandchild',
+          parent: 1,
+          uid: 2,
+        },
+        3: {
+          name: 'Child',
+          parent: 0,
+          uid: 3,
+        },
+        4: {
+          name: 'Grandchild',
+          parent: 3,
+          uid: 4,
+        },
+        5: {
+          name: 'Grandchild',
+          parent: 0,
+          uid: 5,
+        },
+      }
+    )
   })
 })
--- src/getComponentHierarchy.ts
+++ src/getComponentHierarchy.ts
@@ -3,13 +3,42 @@ import { App, ComponentInternalInstance, ComponentPublicInstance, VNode } from '
 // https://github.com/lmiller1990/vdom-traverse-article/blob/master/index.js
 // https://github.com/vuejs/vue-devtools/blob/f3c48017c82bc1c2357f969647dd13de98017239/packages/app-backend-vue3/src/components/tree.ts#L46
 
-export const getComponentHierarchy = (app: ComponentPublicInstance) => {
-  return traverse(app.$.subTree)
-}
-
 interface FoundComponent {
+  uid: number
+  parent: number // uid
+  name: string
 }
 
-const traverse = (vnode: VNode, found: FoundComponent[]) => {
-  if (vnode
+export type FoundComponents = Record<string, FoundComponent>
+
+export const getComponentHierarchy = (app: ComponentPublicInstance) => {
+  return traverse(app.$.subTree, {})
+}
+
+const traverse = (vnode: VNode, found: FoundComponents): FoundComponents => {
+  if (Array.isArray(vnode.children)) {
+    return vnode.children.reduce<FoundComponents>((acc, curr) => {
+      if (typeof curr === 'object') {
+        let node = curr as VNode
+
+        if (!node.component || !node.component.uid) {
+          throw Error('WTF')
+        }
+
+        acc = {
+          ...acc,
+          [node.component.uid]: {
+            uid: node.component.uid,
+            name: node.component.type.name,
+            parent: node.component.parent!.uid
+          }
+        }
+        return traverse(node.component.subTree, acc)
+      }
+      
+      return acc
+    }, {...found})
+  }
+
+  return found
 }
